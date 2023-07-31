a6ec5076489cfb3de103bc794ab11b5593c5c46f
Jonathan <jonathanelian64@gmail.com>
2023-07-31 10:30:08 -0600

    add Unwrap method and add documentation

--- option.go
+++ option.go
@@ -6,12 +6,14 @@ type Optionable interface {
 	any | None
 }
 
+// Wrapper for optional value
 type Option[T any] struct {
 	value Optionable
 }
 
 type None struct{}
 
+// Creates a new Option of type T
 func Optional[T any](initial ...T) Option[T] {
 	if len(initial) == 0 {
 		return Option[T]{None{}}
@@ -20,14 +22,17 @@ func Optional[T any](initial ...T) Option[T] {
 	return Option[T]{initial[0]}
 }
 
+// check if the current option value is None
 func (o Option[T]) IsNone() bool {
 	return o.value == None{}
 }
 
+// check if the current option value is Some
 func (o Option[T]) IsSome() bool {
 	return o.value != None{}
 }
 
+// get the current option value and if exists an error
 func (o Option[T]) GetValue() (variable T, err error) {
 	rawValue := o.value
 
@@ -40,15 +45,26 @@ func (o Option[T]) GetValue() (variable T, err error) {
 	}
 }
 
+// get the current option value or default value passed
 func (o Option[T]) ValueOr(defaultVal T) T {
 	if o.IsSome() {
-		value, _ := o.GetValue()
+		value := o.value.(T)
 		return value 
 	} else {
 		return defaultVal
 	}
 }
 
+// get the value or panic the program
+func (o Option[T]) Unwrap() T {
+	if o.IsNone() {
+		panic("option is none")
+	}
+
+	return o.value.(T)
+}
+
+// set the current option value
 func (o Option[T]) SetValue(value Optionable) Option[T] {
 	o.value = value
 	return o
